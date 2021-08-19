c8e9a4f6bf7e16d149793549d596334a0a8472c2
unknown <sankalp.umr@gmail.com>
2021-08-19 20:44:02 +0530

    Emp wage per hour

---
+++ com/src/com/Empwagebuilderif.java
@@ -0,0 +1,25 @@
+package com;
+
+public class Empwagebuilderif {
+	public static void main(String args[]) {
+		
+		int is_part_time = 1;
+		int is_full_time = 2;
+		int emp_rate_per_hour = 20;
+		
+		int emphrs =0;
+		int empwage = 0;
+		
+		double empcheck = Math.floor(Math.random() * 10) % 3;
+			if ( empcheck == is_part_time )
+				emphrs =4;
+		else if (empcheck == is_full_time)
+			emphrs = 8;
+		else
+			emphrs = 0;
+		empwage = emphrs * emp_rate_per_hour;
+		System.out.println("emp wage" + empwage);
+	}
+	
+
+}
