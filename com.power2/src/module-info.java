7b88079dd299bc71d8acd0acfdd1a53e5538c39f
unknown <sankalp.umr@gmail.com>
2021-08-22 14:38:41 +0530

    day 5th 3rd power of 2

---
+++ com.power2/src/module-info.java
@@ -0,0 +1,2 @@
+module com.power2 {
+}
---
+++ com.power2/src/powertwo/Power2.java
@@ -0,0 +1,29 @@
+package powertwo;
+import java.util.Scanner;
+	
+	public class Power2 {
+	
+		  public static void main(String args[]) {
+		 
+		  Scanner a= new Scanner(System.in);
+
+		  System.out.println("plz enter any number");
+		  int num = a.nextInt();
+
+
+		  for(int i=1; i<=num; i++)
+		  {
+
+		  int holdb=1;   
+ 		  holdb=2*i;
+
+ 		  System.out.println(+holdb);
+		  
+	 
+	        }
+
+	    }
+	}
+
+
+
